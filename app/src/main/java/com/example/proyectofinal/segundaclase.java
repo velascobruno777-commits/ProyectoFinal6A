0ca5b2c2bb61be3bd3038a2887571ef33230ad36
Bruno Velasco <velascobruno41@gmail.com>
2026-05-25 08:31:47 -0600

    Entrega

---
+++ app/src/main/java/com/example/proyectofinal/segundaclase.java
@@ -0,0 +1,14 @@
+package com.example.proyectofinal;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class segundaclase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
